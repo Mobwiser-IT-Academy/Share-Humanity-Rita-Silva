7745746143fcde3644bdb423f0d906c7fbe92f6b
RitaSilva28-github <63984197+RitaSilva28@users.noreply.github.com>
2020-10-12 15:41:36 +0100

    fixed the filter

--- JS/share.js
+++ JS/share.js
@@ -1,6 +1,6 @@
 const issues = [{
     type: "Health",
-    class: "issue1",
+    type2: "",
     title: "Coronavirus Pandemic",
     src: "https://executivedigest.sapo.pt/wp-content/uploads/2020/03/coronavirus1.jpg",
     description: "Coronavirus Pandemic is afecting right now more than <strong>12 Million </strong> People around the world.",
@@ -9,7 +9,7 @@ const issues = [{
 
   {
     type: "Social",
-    class: "issue2",
+    type2: "Economic",
     title: "Poverty",
     src: "https://borgenproject.org/wp-content/uploads/Poverty-in-Malawi.jpg",
     description: "More than <strong> 704 Million </strong> people in the world live in extreme poverty.",
@@ -17,8 +17,8 @@ const issues = [{
   },
 
   {
-    type: ["Social", "Economic"],
-    class: "issue3",
+    type: "Economic",
+    type2: "Social",
     title: "Hunger",
     src: "https://www.lowyinstitute.org/sites/default/files/49838682888_f94f9ad9f4_k_0.jpg",
     description: "Around the world, <strong>821 Million </strong> people do not have enough of the food they need to live an active, healthy life",
@@ -26,8 +26,8 @@ const issues = [{
   },
 
   {
-    type: ["Social", "Environment"],
-    class: "issue4",
+    type: "Social",
+    type2: "Environment",
     title: "Water",
     src: "https://www.health.harvard.edu/media/content/images/cr/b8a1309a-ba53-48c7-bca3-9c36aab2338a.jpg",
     description: "<strong> 829 000</strong> people are estimated to die each year from diarrhoea as a result of unsafe drinking-water, sanitation, and hand hygiene.",
@@ -36,7 +36,7 @@ const issues = [{
 
   {
     type: "Environment",
-    class: "issue5",
+    type2: "",
     title: "Climate Change",
     src: "https://images.theconversation.com/files/292578/original/file-20190916-19030-1c91vdb.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=cli",
     description: "<strong>11%</strong> of the world’s population is currently vulnerable to climate change impacts such as droughts, floods, heat waves, extreme weather events and sea-level rise",
@@ -45,7 +45,7 @@ const issues = [{
 
   {
     type: "Social",
-    class: "issue6",
+    type2: "Economic",
     title: "Refugees",
     src: "https://www.theelders.org/sites/default/files/migrated4/syrian-refugee-child-beach-600x400.jpg",
     description: "At least <strong> 79.5 Million </strong> people around the world have been forced to flee their homes. Among them are nearly <strong>26 Million</strong> refugees, around half of whom are under the age of 18",
@@ -53,8 +53,8 @@ const issues = [{
   },
 
   {
-    type: ["Social", "Economic"],
-    class: "issue7",
+    type: "Economic",
+    type2: "Social",
     title: "Gender Equality",
     src: "https://i1.wp.com/www.travindy.com/wp-content/uploads/2017/12/PATA-will-highlight-the-issue-of-gender-equality-in-2018-Dec122017.png?resize=710%2C502&ssl=1",
     description: "Despite progress, gender inequality continues to hold women and girls back and deprive them of basic rights and opportunities.",
@@ -63,7 +63,7 @@ const issues = [{
 
   {
     type: "Social",
-    class: "issue8",
+    type2: "",
     title: "Domestic Violence",
     src: "https://media3.s-nbcnews.com/j/newscms/2018_43/2616181/181023-domestic-violence-stock-ac-618p_5c28e9c89f761909895faa939e650813.fit-2000w.jpg",
     description: "About <strong>1 in 3 (35%) </strong> of women worldwide have experienced either physical and/or sexual intimate partner violence or non-partner sexual violence in their lifetime.",
@@ -76,15 +76,11 @@ const issues = [{
 function createHtml() {
 
   let issuesHtml = '';
-  let array = [];
   for (let i = 0; i < issues.length; i++) {
-
     issuesHtml += `<div id=${issues[i].class}><img src=${issues[i].src}></img><h5>${issues[i].title}</h5> <p>${issues[i].description}</p> <button> <a href=${issues[i].href}>View Detail</a></button></div>`;
-    array.push(issuesHtml);
+
 
   }
-
-
 
   var container = document.getElementById('issues-container');
   container.innerHTML = issuesHtml;
@@ -100,8 +96,7 @@ var health = document.getElementById("health");
 var social = document.getElementById("social");
 var economic = document.getElementById("economic");
 var environment = document.getElementById("environment");
-
-
+var container = document.getElementById('issues-container');
 
 
 showAll.addEventListener("click", function () {
@@ -114,9 +109,11 @@ health.addEventListener("click", function () {
 social.addEventListener("click", function () {
   filterByType("Social");
 
+
 });
 economic.addEventListener("click", function () {
   filterByType("Economic");
+
 });
 environment.addEventListener("click", function () {
   filterByType("Environment");
@@ -124,6 +121,8 @@ environment.addEventListener("click", function () {
 
 
 function filterByType(issue) {
+
+  issuesHtml = '';
 
   let array = [];
 
@@ -133,8 +132,23 @@ function filterByType(issue) {
       array.push(issues[i]);
     }
 
+    if (issue === issues[i].type2) {
+      array.push(issues[i]);
+
+    }
+
+
   }
+
+  for (let i = 0; i < array.length; i++) {
+    issuesHtml += `<div id=${array[i].class}><img src=${array[i].src}></img><h5>${array[i].title}</h5> <p>${array[i].description}</p> <button> <a href=${array[i].href}>View Detail</a></button></div>`
+  }
+
+
+  container.innerHTML = issuesHtml;
   console.log(array)
+
+
 
 };
 
