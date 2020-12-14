17c58aadaba9e4cecbcfdd14bb77cd0f32f9e19a
RitaSilva28-github <63984197+RitaSilva28@users.noreply.github.com>
2020-12-14 23:31:56 +0000

    prettier script working

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,47 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: {
-        index: "./src/index/index.js",
-        coronavirus: "./src/coronavirus/coronavirus.js",
-        detail: "./src/detail/detail.js"
+  mode: "development",
+  entry: {
+    index: "./src/index/index.js",
+    coronavirus: "./src/coronavirus/coronavirus.js",
+    detail: "./src/detail/detail.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: "coronavirus.html",
+      template: "./src/coronavirus/coronavirus.html",
+    }),
 
-    },
-    output: {
-        filename: "[name].bundle.js",
-        path: path.resolve(__dirname, "dist")
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: "detail.html",
+      template: "./src/detail/detail.html",
+    }),
+  ],
 
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index/index.html"
-        }),
-        new HtmlWebpackPlugin({ // Also generate a test.html
-            filename: 'coronavirus.html',
-            template: './src/coronavirus/coronavirus.html'
-        }),
-
-        new HtmlWebpackPlugin({ // Also generate a test.html
-            filename: 'detail.html',
-            template: './src/detail/detail.html'
-        }),
-
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
+  },
 
-    module: {
-        rules: [{
-            test: /\.css$/,
-            use: ["style-loader", "css-loader"]
-        }]
-
-    },
-
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        port: 9000,
-        open: true,
-    }
-
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    open: true,
+  },
 };
