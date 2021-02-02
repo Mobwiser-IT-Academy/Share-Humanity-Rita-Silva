2b7c6a2d7f5dbd77937b955ed895b4720c917ac3
Rita Silva <rita.cfs28@gmail.com>
2021-02-02 21:58:16 +0000

    lint and prettier

--- index.js
+++ index.js
@@ -1,95 +1,94 @@
-
 require("./index.css");
 var issues = [
-    {
-        category: ["Health", "Coronavirus", "Covid", "Corona", "Pandemic"],
-        title: "Coronavirus Pandemic",
-        src:
+  {
+    category: ["Health", "Coronavirus", "Covid", "Corona", "Pandemic"],
+    title: "Coronavirus Pandemic",
+    src:
       "https://executivedigest.sapo.pt/wp-content/uploads/2020/03/coronavirus1.jpg",
-        description:
+    description:
       "Coronavirus Pandemic is afecting right now more than <strong>12 Million </strong> People around the world.",
-        href: "coronavirus.html",
-    },
-    {
-        category: ["Social", "Economic", "Poverty"],
-        title: "Poverty",
-        src: "https://borgenproject.org/wp-content/uploads/Poverty-in-Malawi.jpg",
-        description:
+    href: "coronavirus.html",
+  },
+  {
+    category: ["Social", "Economic", "Poverty"],
+    title: "Poverty",
+    src: "https://borgenproject.org/wp-content/uploads/Poverty-in-Malawi.jpg",
+    description:
       "More than <strong> 704 Million </strong> people in the world live in extreme poverty.",
-        href: "detail.html",
-    },
-    {
-        category: ["Social", "Economic", "Africa", "Hunger", "Food"],
-        title: "Hunger",
-        src:
+    href: "detail.html",
+  },
+  {
+    category: ["Social", "Economic", "Africa", "Hunger", "Food"],
+    title: "Hunger",
+    src:
       "https://www.lowyinstitute.org/sites/default/files/49838682888_f94f9ad9f4_k_0.jpg",
-        description:
+    description:
       "Around the world, <strong>821 Million </strong> people do not have enough of the food they need to live an active, healthy life",
-        href: "detail.html",
-    },
-    {
-        category: ["Social", "Environment", "Planet", "Water", "Climate"],
-        title: "Water",
-        src:
+    href: "detail.html",
+  },
+  {
+    category: ["Social", "Environment", "Planet", "Water", "Climate"],
+    title: "Water",
+    src:
       "https://www.health.harvard.edu/media/content/images/cr/b8a1309a-ba53-48c7-bca3-9c36aab2338a.jpg",
-        description:
+    description:
       "<strong> 829 000</strong> people are estimated to die each year from diarrhoea as a result of unsafe drinking-water, sanitation, and hand hygiene.",
-        href: "detail.html",
-    },
-    {
-        category: ["Environment", "Climate change", "Planet", "climate"],
-        title: "Climate Change",
-        src:
+    href: "detail.html",
+  },
+  {
+    category: ["Environment", "Climate change", "Planet", "climate"],
+    title: "Climate Change",
+    src:
       "https://images.theconversation.com/files/292578/original/file-20190916-19030-1c91vdb.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=cli",
-        description:
+    description:
       "<strong>11%</strong> of the world’s population is currently vulnerable to climate change impacts such as droughts, floods, heat waves, extreme weather events and sea-level rise",
-        href: "detail.html",
-    },
-    {
-        category: [
-            "Social",
-            "Economic",
-            "International",
-            "World",
-            "Refugees",
-            "Refugees crisis",
-        ],
-        title: "Refugees",
-        src:
+    href: "detail.html",
+  },
+  {
+    category: [
+      "Social",
+      "Economic",
+      "International",
+      "World",
+      "Refugees",
+      "Refugees crisis",
+    ],
+    title: "Refugees",
+    src:
       "https://www.theelders.org/sites/default/files/migrated4/syrian-refugee-child-beach-600x400.jpg",
-        description:
+    description:
       "At least <strong> 79.5 Million </strong> people around the world have been forced to flee their homes. Among them are nearly <strong>26 Million</strong> refugees, around half of whom are under the age of 18",
-        href: "detail.html",
-    },
-    {
-        category: ["Social", "Economic", "Gender", "Equality", "Gender equality"],
-        title: "Gender Equality",
-        src:
+    href: "detail.html",
+  },
+  {
+    category: ["Social", "Economic", "Gender", "Equality", "Gender equality"],
+    title: "Gender Equality",
+    src:
       "https://i1.wp.com/www.travindy.com/wp-content/uploads/2017/12/PATA-will-highlight-the-issue-of-gender-equality-in-2018-Dec122017.png?resize=710%2C502&ssl=1",
-        description:
+    description:
       "Despite progress, gender inequality continues to hold women and girls back and deprive them of basic rights and opportunities.",
-        href: "detail.html",
-    },
-    {
-        category: ["Social", "Domestic violence", "Violence"],
-        title: "Domestic Violence",
-        src:
+    href: "detail.html",
+  },
+  {
+    category: ["Social", "Domestic violence", "Violence"],
+    title: "Domestic Violence",
+    src:
       "https://media3.s-nbcnews.com/j/newscms/2018_43/2616181/181023-domestic-violence-stock-ac-618p_5c28e9c89f761909895faa939e650813.fit-2000w.jpg",
-        description:
+    description:
       "About <strong>1 in 3 (35%) </strong> of women worldwide have experienced either physical and/or sexual intimate partner violence or non-partner sexual violence in their lifetime.",
-        href: "detail.html",
-    },
+    href: "detail.html",
+  },
 ];
 function createHtml(filterByCategory) {
-    var issuesHtml = "";
-    for (var i = 0; i < issues.length; i++) {
-        if (
-            !filterByCategory ||
+  var issuesHtml = "";
+  for (var i = 0; i < issues.length; i++) {
+    if (
+      !filterByCategory ||
       issues[i].category.some(function (issue) {
-          return issue === filterByCategory;
+        return issue === filterByCategory;
       })
-        ) {
-            issuesHtml +=
+    ) {
+      issuesHtml +=
         "<div><img src=" +
         issues[i].src +
         "></img><h5>" +
@@ -99,11 +98,11 @@ function createHtml(filterByCategory) {
         "</p> <button> <a href=" +
         issues[i].href +
         ">View Detail</a></button></div>";
-        }
     }
-    console.log(issues);
-    var container = document.getElementById("issues-container");
-    container.innerHTML = issuesHtml;
+  }
+  console.log(issues);
+  var container = document.getElementById("issues-container");
+  container.innerHTML = issuesHtml;
 }
 createHtml("");
 //
@@ -115,34 +114,34 @@ var environment = document.getElementById("environment");
 var container = document.getElementById("issues-container");
 var searchButton = document.getElementById("search-button");
 showAll.addEventListener("click", function () {
-    createHtml("");
+  createHtml("");
 });
 health.addEventListener("click", function () {
-    createHtml("Health");
+  createHtml("Health");
 });
 social.addEventListener("click", function () {
-    createHtml("Social");
+  createHtml("Social");
 });
 economic.addEventListener("click", function () {
-    createHtml("Economic");
+  createHtml("Economic");
 });
 environment.addEventListener("click", function () {
-    createHtml("Environment");
+  createHtml("Environment");
 });
 // Search Bar Filter
 var searchBar = document.getElementById("searchbar");
 var search = searchBar.value;
 searchButton.addEventListener("click", function () {
-    var upperSearch =
+  var upperSearch =
     search.charAt(0).toUpperCase() + search.slice(1).toLowerCase();
-    createHtml(upperSearch);
-    window.location = "#issues-container";
-    console.log(search);
+  createHtml(upperSearch);
+  window.location = "#issues-container";
+  console.log(search);
 });
 searchBar.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-        event.preventDefault();
-        document.getElementById("search-button").click();
-        window.location = "#issues-container";
-    }
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("search-button").click();
+    window.location = "#issues-container";
+  }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -2,56 +2,56 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode: "development",
-    entry: {
-        index: "./src/index/index.ts",
-        coronavirus: "./src/coronavirus/coronavirus.ts",
-        detail: "./src/detail/detail.ts",
-    },
-    output: {
-        filename: "[name].bundle.js",
-        // eslint-disable-next-line no-undef
-        path: path.resolve(__dirname, "dist"),
-    },
+  mode: "development",
+  entry: {
+    index: "./src/index/index.ts",
+    coronavirus: "./src/coronavirus/coronavirus.ts",
+    detail: "./src/detail/detail.ts",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    // eslint-disable-next-line no-undef
+    path: path.resolve(__dirname, "dist"),
+  },
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index/index.html",
-        }),
-        new HtmlWebpackPlugin({
-            // Also generate a test.html
-            filename: "coronavirus.html",
-            template: "./src/coronavirus/coronavirus.html",
-        }),
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index/index.html",
+    }),
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: "coronavirus.html",
+      template: "./src/coronavirus/coronavirus.html",
+    }),
 
-        new HtmlWebpackPlugin({
-            // Also generate a test.html
-            filename: "detail.html",
-            template: "./src/detail/detail.html",
-        }),
+    new HtmlWebpackPlugin({
+      // Also generate a test.html
+      filename: "detail.html",
+      template: "./src/detail/detail.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.tsx?$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
-    },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
 
-    resolve: {
-        extensions: [ ".tsx", ".ts", ".js"],
-    },
-
-    devServer: {
-        contentBase: path.join(__dirname, "dist"),
-        port: 9000,
-        open: true,
-    },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000,
+    open: true,
+  },
 };
