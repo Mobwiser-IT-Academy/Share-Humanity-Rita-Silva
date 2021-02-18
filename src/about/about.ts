31d5ebb333ddb02762d01c992cf31b5021764f04
Rita Silva <rita.cfs28@gmail.com>
2021-02-18 10:45:40 +0000

    arrow function

--- src/about/about.ts
+++ src/about/about.ts
@@ -3,10 +3,10 @@ import "./about.css";
 const button=document.getElementById("arrow-down");
 
 
-button?.addEventListener("click", function(){
+button?.addEventListener("click",() => {
     button.style.opacity="1";
     button.style.animation="none";
-})
+});
 
 
 
