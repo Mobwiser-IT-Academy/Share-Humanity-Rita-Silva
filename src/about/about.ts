4205572615bfb0efcc51591084c3fadef46c52b6
Rita Silva <rita.cfs28@gmail.com>
2021-02-18 00:29:43 +0000

    about us animations and css

--- src/about/about.ts
+++ src/about/about.ts
@@ -1,1 +1,12 @@
 import "./about.css";
+
+const button=document.getElementById("arrow-down");
+
+
+button?.addEventListener("click", function(){
+    button.style.opacity="1";
+    button.style.animation="none";
+})
+
+
+
