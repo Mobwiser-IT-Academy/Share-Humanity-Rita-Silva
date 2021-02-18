c0787ae4698c2ed028d91607ad6f86695b04ec71
Rita Silva <rita.cfs28@gmail.com>
2021-02-18 12:30:07 +0000

    added text and image

--- src/about/about.ts
+++ src/about/about.ts
@@ -1,8 +1,2 @@
 import "./about.css";
 
-const button = document.getElementById("arrow-down");
-
-button?.addEventListener("click", () => {
-  button.style.opacity = "1";
-  button.style.animation = "none";
-});
