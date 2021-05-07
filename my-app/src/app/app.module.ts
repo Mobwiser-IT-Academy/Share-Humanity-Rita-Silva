748b9749e0a189df5eb2c7dbcac77213dd24fde5
Rita Silva <rita.cfs28@gmail.com>
2021-05-07 16:17:45 +0100

    first cponfigurations

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TitleContainerComponent } from './title-container/title-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TitleContainerComponent
   ],
   imports: [
     BrowserModule
