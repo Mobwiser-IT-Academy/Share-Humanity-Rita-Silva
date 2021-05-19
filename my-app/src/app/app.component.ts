b9ae430f5e6b6bb05cbf83311a3eac329ce01f26
Rita Silva <rita.cfs28@gmail.com>
2021-05-19 03:09:36 +0100

    issues generated  but need a lot of corrections

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {IssuesContainerComponent} from 'src/app/issues-container/issues-container.component'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  constructor(){
+    
+  }
+
 }
--- my-app/src/app/issues-container/issues-container.component.ts
+++ my-app/src/app/issues-container/issues-container.component.ts
@@ -1,16 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {ISSUES} from '../mock-issues'
+
+
 
 @Component({
   selector: 'app-issues-container',
   templateUrl: './issues-container.component.html',
-  styleUrls: ['./issues-container.component.css']
+  styleUrls: ['./issues-container.component.css'],
+  
 })
 export class IssuesContainerComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+issues=ISSUES
+
+  ngOnInit():void {
+
+   let issuesHtml='';
   
-  }
+    for(let i=0; i<this.issues.length; i++){
+
+      issuesHtml += `<div><h5>${this.issues[i].title}</h5><img src=${this.issues[i].src}></img> <p>${this.issues[i].description}</p> <button> <a href=${this.issues[i].href}>View Detail</a></button></div>`;
+    }
+
+    let container = (<HTMLElement>document.getElementById("issues-container"));
+
+container.innerHTML = issuesHtml;
+}
+
+
+
 
 }
+
+  
+
+
