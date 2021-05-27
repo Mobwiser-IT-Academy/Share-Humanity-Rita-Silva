d4c1ae894eb29f7549722215d95941479a5c17d6
Rita Silva <rita.cfs28@gmail.com>
2021-05-27 19:15:31 +0100

    filter and issues container modifications

--- my-app/src/app/Home/issues-container/issues-container.component.ts
+++ my-app/src/app/Home/issues-container/issues-container.component.ts
@@ -12,16 +12,53 @@ import {Issues} from '../../mock-issues'
 export class IssuesContainerComponent implements OnInit {
 
 
-issues=Issues
 
+issues=Issues;
 
 
   ngOnInit():void {
 
+  
 
+    
 
 
 }
+
+public static createHtml(issues:any, filterByCategory:string){
+
+  let html = "";
+
+    issues.forEach((issue: IssuesContainerComponent) => {
+      if (
+        !filterByCategory ||
+        issues.category.some((issue:any) => issue === filterByCategory)
+      ) {
+        html += `<div class="box">
+        <img src=${issues.src} /><h5>${issues.title}</h5> <p>${issues.description1} <strong>${issues.strong1}</strong> ${issues.description2} <strong>${issues.strong2}</strong> ${issues.description3}</p> <button><a href=${issues.href}>View Detail</a></button>
+        
+        </div>`;
+
+        const container = <HTMLElement>(
+          document.getElementById("issues-container")
+        );
+
+        container.innerHTML = html;
+      }
+    });
+
+}
+
+public static clearSearchbar(){
+  const searchbar = <HTMLInputElement>document.getElementById("searchbar");
+  searchbar.value = "";
+
+}
+
+
+
+
+
 
   
 }
