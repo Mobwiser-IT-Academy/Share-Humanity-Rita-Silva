524ce2224ef1007a5184a279a8804301efa1b434
Rita Silva <rita.cfs28@gmail.com>
2021-08-23 17:25:00 +0100

    google maps configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -13,6 +14,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { FilterPipe } from './home/filter.pipe';
+import { MapComponent } from './map/map.component';
 
 
 @NgModule({
@@ -24,13 +26,17 @@ import { FilterPipe } from './home/filter.pipe';
     ContactComponent,
     NavbarComponent,
     FooterComponent,
-    FilterPipe
+    FilterPipe,
+    MapComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey:'AIzaSyCIMWVMQ8xGLZYIqpnqGOZhyD4J1wIhgu4'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
